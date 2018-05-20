3241c194dc67e026868f4cc9f3803deca494106c
KeithBeatTheBeast <keithgmills34@gmail.com>
2018-05-19 23:46:20 -0600

    Tests with proper comments

--- asoiafGenotype.pl
+++ asoiafGenotype.pl
@@ -1,11 +1,13 @@
 % asoiafGenotype.pl
 % May 16th, 2018
 % Author: Keith Mills
+
 % Example ruleset for eyes and hair.
 % Lets use George R. R. Martins ASOIAF!
+% Book fans - please do not rip me to shreds over this.
 
 % Most general rule: Anything is dominant with itself.
-% After that, there is a color of eyes (brown) and hair (black)
+% After that, there is a color of eyes (baratheonBlue) and hair (baratheonBlack)
 % that is dominant over all else.
 % Then the individual rules amongst the other recessive genes e.g. blue vs green eyes.
 
@@ -24,37 +26,42 @@
 genes([eyes, hair]).
 
 eyes(X, X, X).
-eyes(brown, _, brown). % Brown eyes dominant all cases,
-eyes(green, blue, green). % Green dominates blue
+eyes(baratheonBlue, _, baratheonBlue). % Baratheon eyes dominant all cases,
+eyes(green, blue, green). % Green dominates regular blue
 eyes(blue, purple, blue). % Blue dominates purple
 eyes(green, purple, purple). % Purple dominates green
 
+eyes(X, X, X).
+eyes(baratheonBlue, _, baratheonBlue). % Baratheon blue dominant all cases.
+eyes(lannisterGreen, andalBlue, lannisterGreen).
+eyes(andalBlue, valyrianViolet, valyrianViolet). % I recall a few Targs marrying blue-eyed Andals and the children having violet eyes.
+eyes(lannisterGreen, valyrianViolet, lannisterGreen).
 
 hair(X, X, X).
-hair(black, _, black). % Black dominates all
-hair(brown, blonde, brown). % Brown over blonde
-hair(brown, red, brown). % Brown over red
-hair(brown, silver, brown). % Brown over silver
-hair(blonde, red, blonde). % Blonde over red
-hair(blonde, silver, silver). % Silver over blonde
-hair(red, silver, red). % Red over silver
+hair(baratheonBlack, _, baratheonBlack). % Baratheon hair dominates all
+hair(firstMenBrown, lannisterGold, firstMenBrown). % Brown over blonde
+hair(firstMenBrown, tullyRed, firstMenBrown). % Brown over red
+hair(firstMenBrown, valyrianSilver, firstMenBrown). % Brown over silver
+hair(lannisterGold, tullyRed, lannisterGold). % Blonde over red
+hair(lannisterGold, valyrianSilver, valyrianSilver). % Silver over blonde
+hair(tullyRed, valyrianSilver, tullyRed). % Red over silver
 
 % The Mother of Madness
-person(cersei, eyes, green, blue).
-person(cersei, hair, blonde, blonde).
+person(cersei, eyes, lannisterGreen, lannisterGreen).
+person(cersei, hair, lannisterGold, lannisterGold).
 
 % The Kingslayer
-person(jaime, eyes, green, green).
-person(jaime, hair, blonde, blonde).
+person(jaime, eyes, lannisterGreen, lannisterGreen).
+person(jaime, hair, lannisterGold, lannisterGold).
 
 % The Usurper King
-person(robert, eyes, blue, purple).
-person(robert, hair, black, brown).
+person(robert, eyes, baratheonBlue, _).
+person(robert, hair, baratheonBlack, firstMenBrown).
 
 % Aerys III
-person(joffrey, eyes, green, green).
-person(joffrey, hair, blonde, blonde).
+person(joffrey, eyes, lannisterGreen, lannisterGreen).
+person(joffrey, hair, lannisterGold, lannisterGold).
 
 % Orys Baratheon reborn, or the One Trueborn Heir...?
-person(gendry, eyes, blue, blue).
-person(gendry, hair, black, blonde).
+person(gendry, eyes, baratheonBlue, _).
+person(gendry, hair, baratheonBlack, _).
--- phenotypeSelector.pl
+++ phenotypeSelector.pl
@@ -3,25 +3,28 @@
 % Author: Keith Mills
 
 % This is just a simple SWI Prolog program I decided to make in my free time to 
-% Determine the possible phenotypes of offspring given a specified ruleset.
-% It is mainly for fictional characters to see if a universe obeys simple genetics by punnett square logic.
-% Does not have much code yet, just a few tests.
-% To run this code and the current tests with SWI Prolog, you will need to consult 
-% genotypeInformation.pl
-% which contains the ruleset for dominant/recessive genes (can be arbitrary, whatever you desire)
+% determine the possible phenotypes of offspring given a specified ruleset.
+% This is not meant to be used on real-life genetics I am sure real-life genetics are more complicated than that.
+% Rather, its loosely based on testing the concept of "Magic A Is Magic A"
+% http://tvtropes.org/pmwiki/pmwiki.php/Main/MagicAIsMagicA
+% but applied to the genetics of a universe.
 
-% Assuming consult(genotypeInformation) has been invoked...
-a1(N) :- eyes(brown, brown, N). % N = brown
-a2(N) :- eyes(blue, brown, N). % N = brown, not working right now.
-a3(N) :- eyes(brown, blue, N). % N = brown, does work.
-a3 :- eyes(brown, brown, blue). % False
+% It is mainly for fictional characters to see if their made-up universe obeys simple genetics by punnett square logic.
+% To run this code and the current tests with SWI Prolog, you will need to consult 
+% a sample genotype file (ruleset), containing a list of relevant genes, dominant/recessive rules, 
+% and individuals with their traits.
+% A sample genotype file is provided, asoiafGenotype.pl, alongside test cases in testsASOIAF.pl
 
 % TODO:
 % A function, potentialPhenos(+Father, +Mother, -Eyes, -Hair), that takes the name of two individuals in the DB and lists all valid phenotype combinations.
 % A function, possiblePhenos(+Father, +Mother, +Child) that takes three individuals in: father, mother and chiid, and reports if the child is possible
 
 % printPheno(+Name).
-% Takes in the name of the individual as an argument, and reports 
+% Takes in the name of the individual as an argument.
+% It then recursively goes through the list of genes declared in 
+% the genes predicate of the genotype file you are consulting.
+% For each gene, it will find the dominant allele, and list it.
+% If no information for said individual on that gene is found, a message is returned stating as such.
 printPheno(Name) :- genes(Genotype), format('~w:', [Name]), printPheno(Name, Genotype), !.
 printPheno(_, []).
 printPheno(Name, [Gcar|Gcdr]) :- dominantGene(Gcar, Name, Pheno), !, format('~n~w of ~w', [Pheno, Gcar]), printPheno(Name, Gcdr).
